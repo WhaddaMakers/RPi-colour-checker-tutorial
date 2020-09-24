5f90eadb0f4c1939ddbd56598534af658327086f
Midas <midasgossye@hotmail.com>
2020-09-24 16:52:48 +0200

    Added -y option install_tts script

--- install_tts.sh
+++ install_tts.sh
@@ -4,4 +4,4 @@ echo "====================================="
 sudo wget -q https://ftp-master.debian.org/keys/release-10.asc -O- | sudo apt-key add -
 sudo su -c "echo 'deb http://deb.debian.org/debian buster non-free' >> /etc/apt/sources.list"
 sudo apt update
-sudo apt install libttspico-utils
+sudo apt install -y libttspico-utils

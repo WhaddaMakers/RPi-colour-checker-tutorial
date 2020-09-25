825e86a6f4f9c1780464285376a9494eba2f75ab
Midas <midasgossye@hotmail.com>
2020-09-25 10:30:24 +0200

    Added comments

--- whadda_you_see.py
+++ whadda_you_see.py
@@ -61,18 +61,41 @@ GPIO.setmode(GPIO.BCM) # Use processor pin numbering system
 NUM_CYCLES = 10 # number of measurements to perform to determine colour
 
 # pin connections
-S0 = 5
-S1 = 6
-S2 = 13
-S3 = 19
-Out = 26
-Button = 20
-##################
+S0 = 5      # S0 colour sensor
+S1 = 6      # S1 colour sensor
+S2 = 13     # 20 colour sensor
+S3 = 19     # S3 colour sensor
+Out = 26    # Out colour sensor
+Button = 20 # Button
+###############################
 
 i2c = busio.I2C(SCL, SDA) # Initialize the I2C bus for the OLED screen
 
 oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c) # Initialize OLED screen (128x64 pixels)
 oled.fill(0) # fill the screen with 0's (clear screen)
+
+# setup GPIO pins (inputs/outputs)
+GPIO.setup(S0, GPIO.OUT)
+GPIO.setup(S1, GPIO.OUT)
+GPIO.setup(S2, GPIO.OUT)
+GPIO.setup(S3, GPIO.OUT)
+GPIO.setup(Out, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(Button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#####################################################
+
+# pin reading sequence for Red, Green, Blue measurements [S2, S3]
+pin_reading_seq = [[GPIO.LOW, GPIO.LOW], [GPIO.HIGH, GPIO.HIGH], [GPIO.LOW, GPIO.HIGH], [GPIO.HIGH, GPIO.LOW]]
+##############################################################################################################
+
+
+RGBW = [0, 0, 0, 0] # Initialize empty array to hold Red, Green, Blue & White colour sensor readings
+
+# Set colour frequency scaling to 2% / 12 kHz
+GPIO.output(S0, GPIO.LOW)
+GPIO.output(S1, GPIO.HIGH)
+
+colour_names = ['Red', 'Green', 'Blue', 'White']
+
 
 # Show Raspberry Pi Logo on OLED screen for 1 sec
 image = Image.open("RPI.bmp") 
@@ -113,104 +136,133 @@ draw.text((70, 10), "Sat.: ", font=font, fill=255, align="right")
 draw.text((70, 20), "Value: ", font=font, fill=255, align="right")
 ##################################################################
 
-draw.line((64, 0, 64, 32), width=1, fill=255)
-draw.line((0, 32, 127, 32), width=1, fill=255)
+draw.line((64, 0, 64, 32), width=1, fill=255)  # draw vertical seperation line
+draw.line((0, 32, 127, 32), width=1, fill=255) # draw horizontal seperation line
 
-# Display image
+# Update image on OLED
 oled.image(image)
 oled.show()
-# set pin 
-GPIO.setup(S0, GPIO.OUT)
-GPIO.setup(S1, GPIO.OUT)
-GPIO.setup(S2, GPIO.OUT)
-GPIO.setup(S3, GPIO.OUT)
-GPIO.setup(Out, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(Button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+######################
 
-pin_reading_seq = [[GPIO.LOW, GPIO.LOW], [GPIO.HIGH, GPIO.HIGH], [GPIO.LOW, GPIO.HIGH], [GPIO.HIGH, GPIO.LOW]]
-RGBW = [0, 0, 0, 0]
+"""
+Function descrition:
+    Reads the Red, Green, Blue & White colour components from the TCS3200 colour sensor,
+    converts the RGB colour into the HSV colour space, determines the colour name from the Hue value,
+    and displays all of the above information on the OLED screen.
 
-GPIO.output(S0, GPIO.LOW)
-GPIO.output(S1, GPIO.HIGH)
+Arguments:
+    None
 
-colour_names = ['Red', 'Green', 'Blue', 'White']
-
-
+Returns:
+    -color_str (String with the colour name)
+    -HSV_Val (list with Hue, Saturation and Value components respectively)
+"""
 def read_color():
     for idx, pin_states in enumerate(pin_reading_seq):
+        # Select colour filter
         GPIO.output(S2, pin_states[0])
         GPIO.output(S3, pin_states[1])
+        ##############################
         time.sleep(0.01)
 
-        start = time.time()
+        start = time.time() # start timing measurement to time pulse length
+
+        # Wait until the NUM_cycles pulses have been counted
         for impulse_count in range(NUM_CYCLES):
             GPIO.wait_for_edge(Out, GPIO.FALLING)
-        duration = time.time() - start
-        RGBW[idx] = ((NUM_CYCLES / duration) / 10000) *255
+        ####################################################
 
-    RGBW[0] = (RGBW[0] /RGBW[3])
+        duration = time.time() - start # calculate duration of pulses
+        RGBW[idx] = ((NUM_CYCLES / duration) / 10000) *255 # compute Red, Green and White colour components and scale to 0-255
+
+    # Scale Red, Green & Blue components with respect to the White/Brightness reading
+    RGBW[0] = (RGBW[0] /RGBW[3]) 
     RGBW[1] = (RGBW[1] / RGBW[3])
     RGBW[2] = (RGBW[2] / RGBW[3])
+    #################################################################################
 
-    HSV_val = colorsys.rgb_to_hsv(RGBW[0], RGBW[1], RGBW[2])
+    HSV_val = colorsys.rgb_to_hsv(RGBW[0], RGBW[1], RGBW[2]) # Convert RGB colour readings into HSV colour space
+    
     color_str = ""
     
+    # Convert Red, Green & Blue values to rounded percentages
     R_perc = round(RGBW[0]*100)
     G_perc = round(RGBW[1]*100)
     B_perc = round(RGBW[2]*100)
-    hue_deg = round(HSV_val[0]*360)
+    #########################################################
+
+    hue_deg = round(HSV_val[0]*360) # Convert Hue to rounded degrees
+
+    # Convert value and saturation to rounded percentages
     val_perc = round(HSV_val[1]*100)
     sat_perc = round(HSV_val[2]*100)
+    #####################################################
 
+    # draw "black" rectangle to clear screen space for updated numbers
     draw.rectangle((34, 0, 63, 30), fill=0)
     draw.rectangle((100, 0, 127, 30), fill=0)
+    ###################################################################
+
+    # Put updated Red, Green and Blue percentages on screen
     draw.text((30, 0), "{:3d} %".format(R_perc), font=font, fill=255, align="right")
     draw.text((30, 10), "{:3d} %".format(G_perc), font=font, fill=255, align="right")
     draw.text((30, 20), "{:3d} %".format(B_perc), font=font, fill=255, align="right")
+    #################################################################################
 
+    # Put updated Hue, value and saturation numbers on screen
     draw.text((100, 0), "{:3d} °".format(hue_deg), font=font, fill=255, align="right")
     draw.text((100, 10), "{:3d} %".format(val_perc), font=font, fill=255, align="right")
     draw.text((100, 20), "{:3d} %".format(sat_perc), font=font, fill=255, align="right")
+    ####################################################################################
     
-    if HSV_val[2] < 0.35:
+    # Determine colour based on hue parameter
+    if sat_perc < 35:
         color_str = "Black/Dark"
-    elif HSV_val[0] > (357/360) or HSV_val[0] < (10/360):
+    elif hue_deg > 357 or hue_deg < 10:
         color_str = "ORANGE"
-    elif HSV_val[0] > (10/360) and HSV_val[0] < (40/360):
+    elif hue_deg > 10 and hue_deg < 40:
         color_str = "YELLOW"
-    elif HSV_val[0] > (45/360) and HSV_val[0] < (160/360):
+    elif hue_deg > 45 and hue_deg < 160:
         color_str = "GREEN"
-    elif HSV_val[0] > (210/360) and HSV_val[0] < (270/360):
+    elif hue_deg > 210 and hue_deg < 270:
         color_str = "BLUE"
-    elif HSV_val[0] > (270/360) and HSV_val[0] < (330/360):
+    elif hue_deg > 270 and hue_deg < 330:
         color_str ="PURPLE"
-    elif HSV_val[0] > (340/360) and HSV_val[0] < (356/360):
+    elif hue_deg > 340 and hue_deg < 356:
         color_str = "RED"
     else:
         color_str = "No valid color detected"
+    #########################################
 
-    draw.rectangle((0, 34, 128, 60), fill=0)
+    draw.rectangle((0, 34, 128, 60), fill=0) # clear bottom part of screen by drawing "black" rectangle
+
+    # if a valid color is detected...
     if color_str != "No valid color detected" and color_str != "Black/Dark":
-        draw.rectangle((0, 34, 128, 60), fill=255)
-
-        w, h = draw.textsize(color_str, font=font2)
-
-
-        draw.text(((128-w)/2, 33), color_str, font=font2, fill=0)
+        draw.rectangle((0, 34, 128, 60), fill=255)                  # Draw a filled rectangle as background
+        w, h = draw.textsize(color_str, font=font2)                 # Calculate pixel width and height of color name
+        draw.text(((128-w)/2, 33), color_str, font=font2, fill=0)   # Put color name on screen with inversed text
         
-    oled.image(image)
+    # Update OLED screen with new image
+    oled.image(image) 
     oled.show()
+    ###################################
 
     return color_str, HSV_val
 
 while True:
-    color_str, HSV_val = read_color()
+    color_str, HSV_val = read_color() # Read and display colour name and values
+
+    # If button is pressed...
     if GPIO.input(Button) == GPIO.LOW:        
+        # Print colour details on terminal
         print("Detected color: {}".format(color_str))
         print("Hue:", HSV_val[0]*360)
         print("\n")
+        ##################################
  
+        # Using the Pico Text-To-Speech program SAY the colour
         os.system('pico2wave -w tts.wav "{}" && aplay tts.wav'.format(color_str))
+        
         print("=================")
         print("\n")
         
